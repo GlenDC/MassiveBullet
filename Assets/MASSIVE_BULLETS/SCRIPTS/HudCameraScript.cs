2d374d458ecde682d27827863f11e35073953211
GitBouckx <guillaumebouckaert@Guillaumes-MacBook-Pro.local>
2014-07-19 03:04:24 +0200

    created HUD camera prefab and associated scripts

---
+++ Assets/MASSIVE_BULLETS/SCRIPTS/HudCameraScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudCameraScript : MonoBehaviour {
+
+	[ SerializeField ] GameObject
+		PistolPrefab;
+
+	GameObject
+		PistolObject;
+
+	[ SerializeField ] Vector3
+		PistolPosition;
+
+	[ SerializeField ] Quaternion
+		PistolOrientation;
+
+	// Use this for initialization
+	void Start () {
+
+		PistolObject = Instantiate(PistolPrefab,PistolPosition,Quaternion.identity) as GameObject;
+		PistolObject.transform.parent = this.transform;
+		PistolObject.name = "HUD_PistolObject";
+
+		//PistolObject.GetComponent<PistolObjectScript>().SetUpPistolObject();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
