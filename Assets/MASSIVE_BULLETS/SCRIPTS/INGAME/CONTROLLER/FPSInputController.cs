d508206dff23e7d3e8a40504016d7eca4c06181c
Glen De Cauwsemaecker <glen.decauwsemaecker@fishingcactus.com>
2014-07-19 04:05:58 +0200

    changed controller type

---
+++ Assets/MASSIVE_BULLETS/SCRIPTS/INGAME/CONTROLLER/FPSInputController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+
+public class FPSInputController : MonoBehaviour
+{
+    CharacterMotor
+        motor;
+
+    Config
+        configScript;
+
+    void Start()
+    {
+        motor =
+            GetComponent< CharacterMotor >();
+
+        configScript =
+            GameObject.FindWithTag( TAGS.CONFIG ).GetComponent< Config >();
+    }
+
+    void Update()
+    {
+        Vector3
+            directionVector;
+
+        directionVector = Vector3.zero;
+        directionVector.x = configScript.GetStrafeMovement();
+        directionVector.z = configScript.GetLateralMovement();
+
+        if( directionVector != Vector3.zero )
+        {
+            // Get the length of the directon vector and then normalize it
+            // Dividing by the length is cheaper than normalizing when we already have the length anyway
+            float directionLength = directionVector.magnitude;
+            directionVector = directionVector / directionLength;
+            
+            // Make sure the length is no bigger than 1
+            directionLength = Mathf.Min( 1, directionLength );
+            
+            // Make the input vector more sensitive towards the extremes and less sensitive in the middle
+            // This makes it easier to control slow speeds when using analog sticks
+            directionLength = directionLength * directionLength;
+            
+            // Multiply the normalized direction vector by the modified length
+            directionVector = directionVector * directionLength;
+        }
+        
+        // Apply the direction to the CharacterMotor
+        motor.inputMoveDirection = transform.rotation * directionVector;
+        motor.inputJump = Input.GetButton( "Jump" );
+    }
+}
--- Assets/MASSIVE_BULLETS/SCRIPTS/MENU/Config.cs
+++ Assets/MASSIVE_BULLETS/SCRIPTS/MENU/Config.cs
@@ -21,4 +21,38 @@ public class Config : MonoBehaviour
                 "MASSIVE_BULLET_PF_STATE_KEYBOARD", 2
                 );
     }
+
+    public float GetLateralMovement()
+    {
+        float
+            movement;
+
+        if( KeyboardState == KEYBOARD_STATE.AZERTY )
+        {
+            movement = Input.GetKey( KeyCode.Z ) ? 1.0f : 0.0f;
+        }
+        else
+        {
+            movement = Input.GetKey( KeyCode.W ) ? 1.0f : 0.0f;
+        }
+
+        return movement - ( Input.GetKey( KeyCode.S ) ? 1.0f : 0.0f );
+    }
+
+    public float GetStrafeMovement()
+    {
+        float
+            movement;
+
+        if( KeyboardState == KEYBOARD_STATE.AZERTY )
+        {
+            movement = Input.GetKey( KeyCode.Q ) ? -1.0f : 0.0f;
+        }
+        else
+        {
+            movement = Input.GetKey( KeyCode.A ) ? -1.0f : 0.0f;
+        }
+
+        return movement + ( Input.GetKey( KeyCode.D ) ? 1.0f : 0.0f );
+    }
 }
