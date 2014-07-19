2629cd6c5f5e1cbc19427339812f0309b2f952e6
GitBouckx <guillaumebouckaert@Guillaumes-MacBook-Pro.local>
2014-07-19 03:50:12 +0200

    added a generator of randomised obstacles

---
+++ Assets/MASSIVE_BULLETS/SCRIPTS/ObstacleObjectScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleObjectScript : MonoBehaviour {
+
+	[ SerializeField ] GameObject
+		ObstacleObjectCube;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void SetUpObstacleObject(){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
