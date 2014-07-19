eb69b1598c23d97834c6ad575faeba12db55332c
GitBouckx <guillaumebouckaert@Guillaumes-MacBook-Pro.local>
2014-07-19 04:14:49 +0200

    added crosshair to the HUD

--- Assets/MASSIVE_BULLETS/SCRIPTS/HudCameraScript.cs
+++ Assets/MASSIVE_BULLETS/SCRIPTS/HudCameraScript.cs
@@ -15,18 +15,38 @@ public class HudCameraScript : MonoBehaviour {
 	[ SerializeField ] Quaternion
 		PistolOrientation;
 
+	[ SerializeField ] Texture2D
+		CrosshairTexture;
+
 	// Use this for initialization
 	void Start () {
+
+	}
+
+	public void SetUpHUDCamera(bool gun_to_right){
+
+		if (!gun_to_right){
+
+			PistolPosition.x -= PistolPosition.x * 2;
+			PistolOrientation.x += 2 * Mathf.PI;
+		}
 
 		PistolObject = Instantiate(PistolPrefab,PistolPosition,Quaternion.identity) as GameObject;
 		PistolObject.transform.parent = this.transform;
 		PistolObject.name = "HUD_PistolObject";
-
-		//PistolObject.GetComponent<PistolObjectScript>().SetUpPistolObject();
+		
+		PistolObject.GetComponent<PistolObjectScript>().SetUpPistolObject();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnGUI(){
+
+
+
+		GUI.Box (new Rect(10,10,Screen.width/2,Screen.height/2), new GUIContent(CrosshairTexture));
 	}
 }
