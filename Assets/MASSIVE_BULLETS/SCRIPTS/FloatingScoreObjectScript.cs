605d32b1f934744c7c6607b95a9d6daf0d79e72d
GitBouckx <guillaumebouckaert@Guillaumes-MacBook-Pro.local>
2014-07-19 16:26:35 +0200

    added a Prefab effect for the death of a bullet

---
+++ Assets/MASSIVE_BULLETS/SCRIPTS/FloatingScoreObjectScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingScoreObjectScript : MonoBehaviour {
+
+	[ SerializeField ] GameObject
+		FloatingScoreObject;
+
+	[ SerializeField ] float
+		FloatingSpeed,
+		FadingSpeed;
+
+	bool
+		FloatingScoreOn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void SetUpFloatingScore(int score_value, Material text_color){
+
+		FloatingScoreObject.GetComponent<TextMesh>().text = "+ " + score_value.ToString();
+
+		FloatingScoreObject.GetComponent<TextMesh>().color = text_color.color;
+
+		FloatingScoreOn = true;
+	}
+
+	public bool GetFloatingScoreOn(){
+		bool floating_score_on = FloatingScoreOn;
+		return floating_score_on;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (FloatingScoreOn){
+
+			Vector3 floating_score_position = this.transform.position;
+
+			floating_score_position.y += FloatingSpeed * Time.deltaTime;
+
+			FloatingScoreObject.transform.position = floating_score_position;
+
+			Color floating_score_color = FloatingScoreObject.GetComponent<TextMesh>().color;
+
+			floating_score_color.a -= FadingSpeed;
+
+			if (floating_score_color.a < 0.0f){
+
+				FloatingScoreOn = false;
+			}
+
+			FloatingScoreObject.GetComponent<TextMesh>().color = floating_score_color;
+		}
+	}
+}
--- Assets/MASSIVE_BULLETS/SCRIPTS/ScorePopUpScript.cs
+++ Assets/MASSIVE_BULLETS/SCRIPTS/ScorePopUpScript.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class ScorePopUpScript : MonoBehaviour {
 
+	[ SerializeField ] GameObject
+		FloatingScorePrefab;
+
+	[ SerializeField ] Material[]
+		FloatingScoreColor = new Material[3];
+
+	GameObject
+		FloatingScoreObject;
+
+	bool
+		ScorePopUpOn;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+	public void SetUpScorePopUp(int score_value, int color_value){
+
+		FloatingScoreObject = Instantiate(FloatingScorePrefab,this.transform.position,Quaternion.identity) as GameObject;
+		FloatingScoreObject.transform.parent = this.transform;
+		FloatingScoreObject.name = this.name + "_floating_score_object";
+
+		FloatingScoreObject.GetComponent<FloatingScoreObjectScript>().SetUpFloatingScore(score_value,FloatingScoreColor[color_value]);
+
+		this.GetComponent<ParticleSystem>().Play();
+
+		ScorePopUpOn = true;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (ScorePopUpOn){
+
+			if (!FloatingScoreObject.GetComponent<FloatingScoreObjectScript>().GetFloatingScoreOn()){
+
+				Destroy(this);
+			}
+		}
 	}
 }
